a543d5269794220a538a80e9d071b5d8a98db96f
Thomas Jensen <thomas.jensen_@outlook.com>
2024-04-22 14:54:13 +0200

    Completed exercises

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -6,17 +6,33 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // 1. Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne = Boolean
+
+if (NUM >= LOWER && NUM <= UPPER) {
+  answerOne = true
+} else {
+  answerOne = false
+}
 
 // Task 2
-const STR = null
+const STR = 'Hi'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
 // Set answerTwo to false if it's neither of those
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
-let answerTwo
+let answerTwo = Boolean
+
+const acceptedStrings = ['Hello', 'Goodbye']
+
+if (acceptedStrings.includes(STR)) {
+  answerTwo = true
+} else if (!acceptedStrings.includes(STR)) {
+  answerTwo = false
+} else {
+  console.error(Error)
+}
 
 // Task 3
 const AGE = 0
@@ -33,7 +49,32 @@ const AGE = 0
 // 5-12   | Child
 // 13-19  | Teenager
 // 20+    | Adult
-let answerThree
+let answerThree = String
+
+// I found this function on StackOverflow, but I understand how it works
+// It takes the min and max values that I want for the array and adds all the values in between to a new array in addition to the minimum value
+const ageArray = (min, max) =>
+  [...Array(max - min + 1).keys()].map((i) => i + min)
+
+const ageRanges = {
+  toddler: ageArray(1, 4),
+  child: ageArray(5, 12),
+  teenager: ageArray(13, 19)
+}
+
+if (AGE === 0) {
+  answerThree = 'Baby'
+} else if (ageRanges.toddler.includes(AGE)) {
+  answerThree = 'Toddler'
+} else if (ageRanges.child.includes(AGE)) {
+  answerThree = 'Child'
+} else if (ageRanges.teenager.includes(AGE)) {
+  answerThree = 'Teenager'
+} else if (AGE >= 20) {
+  answerThree = 'Adult'
+} else {
+  console.error(Error)
+}
 
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
