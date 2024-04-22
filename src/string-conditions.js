571cc2a43af5d3d52dac0ccbcb0ce6a61a02af0f
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-04-22 16:29:05 +0100

    Changed some answers.

--- src/string-conditions.js
+++ src/string-conditions.js
@@ -36,11 +36,13 @@ console.log(answerThree)
 
 const STR_FIVE = 'Alexandra' // eslint-disable-line no-unused-vars
 
-let answerFour = false
-if (STR_FIVE.startsWith('A') && STR_FIVE.endsWith('a')) {
-  answerFour = true
+const STR_FIVE_lowerCase = STR_FIVE.toLowerCase();
+console.log(STR_FIVE_lowerCase);
+let answerFour = false;
+if (STR_FIVE_lowerCase.charAt(0) == STR_FIVE_lowerCase.charAt(8)) {
+  answerFour = true;
 } else {
-  answerFour = false
+  answerFour = false;
 }
 console.log(answerFour)
 
@@ -49,8 +51,10 @@ console.log(answerFour)
 
 const STR_SIX = 'Joanna' // eslint-disable-line no-unused-vars
 
+STR_SIX_lowerCase = STR_SIX.toLowerCase();
+
 let answerFive = true
-if (STR_SIX.startsWith('J') && STR_SIX.endsWith('J')) {
+if (STR_SIX_lowerCase.charAt(0) === STR_SIX_lowerCase.charAt(5)) {
   answerFive = true
 } else {
   answerFive = false
