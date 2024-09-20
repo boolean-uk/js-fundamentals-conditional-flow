2dd9079dfbbf46d10629070654c7251f2760e060
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-09-20 14:16:33 +0200

    multiple conditions works

--- src/multiple-conditions.js
+++ src/multiple-conditions.js
@@ -7,9 +7,12 @@ const NUM = 9 // eslint-disable-line no-unused-vars
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
 let answerOne
-
+if (NUM >= LOWER) {
+  answerOne = true
+}
+console.log(answerOne)
 // Task 2
-const STR = null
+const STR = 'Goodbye'
 
 // 2. Use conditional statements to set the answerTwo variable below to true
 // if the STR variable is 'Hello' or 'Goodbye'
@@ -17,9 +20,15 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
+if (STR === 'Hello' || STR === 'Goodbye') {
+  answerTwo = true
+} else {
+  answerTwo = false
+}
 
+console.log(answerTwo, STR)
 // Task 3
-const AGE = 0
+const AGE = 11
 
 // 3. Use conditional statements to set the answerThree variable below to a
 // string value based on what the AGE variable is set to.
@@ -34,7 +43,17 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+if (AGE <= 4) {
+  answerThree = 'Baby'
+} else if (AGE <= 12) {
+  answerThree = 'Child'
+} else if (AGE <= 19) {
+  answerThree = 'Teenager'
+} else if (AGE >= 20) {
+  answerThree = 'Adult'
+}
 
+console.log(answerThree, AGE)
 // Run the test after changing the AGE value to verify you've successfully
 // accounted for each age range
 
